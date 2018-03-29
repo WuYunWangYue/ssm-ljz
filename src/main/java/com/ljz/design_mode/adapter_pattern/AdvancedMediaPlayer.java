2d9862c183604cf6169e559ef6ea4f4f76caa370
刘骏柱 <1120573031@qq.com>
2018-03-29 14:09:29 +0800

    添加适配器设计模式

---
+++ src/main/java/com/ljz/design_mode/adapter_pattern/AdvancedMediaPlayer.java
@@ -0,0 +1,7 @@
+package com.ljz.design_mode.adapter_pattern;
+
+public interface AdvancedMediaPlayer {
+    public void playVlc(String fileName);
+
+    public void playMp4(String fileName);
+}
---
+++ src/main/java/com/ljz/design_mode/adapter_pattern/MediaPlayer.java
@@ -0,0 +1,5 @@
+package com.ljz.design_mode.adapter_pattern;
+
+public interface MediaPlayer {
+    public void play(String audioType, String fileName);
+}
