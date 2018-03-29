2feb56a919d12f33e8a9d51b058ae81aa6370323
刘骏柱 <1120573031@qq.com>
2018-03-29 15:10:23 +0800

    添加单例设计模式

---
+++ src/main/java/com/ljz/design_mode/singleton_pattern/Singleton.java
@@ -0,0 +1,16 @@
+package com.ljz.design_mode.singleton_pattern;
+
+//懒汉式 线程不安全
+public class Singleton {
+    private static Singleton instance;
+
+    private Singleton() {
+    }
+
+    public static Singleton getInstance() {
+        if (instance == null) {
+            instance = new Singleton();
+        }
+        return instance;
+    }
+}  
