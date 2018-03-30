3aba72bd42872ce177637da733f4c1711450d4a2
刘骏柱 <1120573031@qq.com>
2018-03-30 18:04:26 +0800

    添加spring-security相关demo

---
+++ src/main/java/com/ljz/security/tool/UrlMatcher.java
@@ -0,0 +1,11 @@
+package com.ljz.security.tool;
+
+public interface UrlMatcher {
+    Object compile(String paramString);
+
+    boolean pathMatchesUrl(Object paramObject, String paramString);
+
+    String getUniversalMatchPattern();
+
+    boolean requiresLowerCaseUrl();
+}
