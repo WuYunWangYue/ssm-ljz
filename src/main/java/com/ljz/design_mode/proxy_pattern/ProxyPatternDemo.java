43e8572e9999f7ff66c74728b79ac753d6584493
刘骏柱 <1120573031@qq.com>
2018-03-30 11:43:11 +0800

    添加策略模式

--- src/main/java/com/ljz/design_mode/proxy_pattern/ProxyPatternDemo.java
+++ src/main/java/com/ljz/design_mode/proxy_pattern/ProxyPatternDemo.java
@@ -6,9 +6,9 @@ public class ProxyPatternDemo {
       Image image = new ProxyImage("test_10mb.jpg");
 
       //图像将从磁盘加载
-      image.display(); 
+      image.display();
       System.out.println("==========================");
       //图像将无法从磁盘加载
-      image.display();     
+      image.display();
    }
 }
---
+++ src/main/java/com/ljz/design_mode/strategy_pattern/OperationMultiply.java
@@ -0,0 +1,9 @@
+
+package com.ljz.design_mode.strategy_pattern;
+
+public class OperationMultiply implements Strategy{
+   @Override
+   public int doOperation(int num1, int num2) {
+      return num1 * num2;
+   }
+}
