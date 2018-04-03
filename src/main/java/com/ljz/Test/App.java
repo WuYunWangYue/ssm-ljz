78a2091680d1274a9b1390bd7ca0445382d616e2
刘骏柱 <1120573031@qq.com>
2018-04-03 11:43:15 +0800

    添加 lombok 包

---
+++ src/main/java/com/ljz/Test/App.java
@@ -0,0 +1,25 @@
+package com.ljz.Test;
+
+import com.ljz.model.Person;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.support.ClassPathXmlApplicationContext;
+
+public class App
+{
+    public static void main( String[] args )
+    {
+    	ApplicationContext context =
+    	  new ClassPathXmlApplicationContext(new String[] {"spring-mybatis.xml"});
+
+    	Person person = (Person)context.getBean("PersonBean");
+
+    	System.out.println("=====================================");
+
+
+    	System.out.println("userName = " + person.getUserName());
+    	System.out.println("password = " + person.getPassword());
+    	System.out.println("age = " + person.getAge());
+    	System.out.println("person = " + person);
+
+    }
+}
---
+++ src/main/java/com/ljz/model/Person.java
@@ -0,0 +1,15 @@
+package com.ljz.model;
+
+import lombok.Getter;
+import lombok.Setter;
+import lombok.ToString;
+
+@Setter
+@Getter
+@ToString
+public class Person {
+    private String userName;
+    private String password;
+    private String age;
+
+}
