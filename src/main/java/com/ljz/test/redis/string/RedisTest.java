7a19f7f3eaf3a5cc9fe669ee230da5cd4d78484f
刘骏柱 <1120573031@qq.com>
2018-04-13 16:23:22 +0800

    redis访问加密

--- src/main/java/com/ljz/test/redis/string/RedisTest.java
+++ src/main/java/com/ljz/test/redis/string/RedisTest.java
@@ -6,6 +6,7 @@ public class RedisTest {
     public static void main(String[] args) {
         //连接本地的 Redis 服务
         Jedis jedis = new Jedis("localhost");
+        jedis.auth("123456");
         System.out.println("连接成功");
         //查看服务是否运行
         System.out.println("服务正在运行: " + jedis.ping());
--- src/test/java/com/ljz/RedisTest.java
+++ src/test/java/com/ljz/RedisTest.java
@@ -4,10 +4,7 @@ import com.ljz.common.util.RedisUtil;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.data.redis.core.HashOperations;
-import org.springframework.data.redis.core.ListOperations;
-import org.springframework.data.redis.core.RedisTemplate;
-import org.springframework.data.redis.core.ValueOperations;
+import org.springframework.data.redis.core.*;
 import org.springframework.test.context.ContextConfiguration;
 import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
 import org.springframework.test.context.web.WebAppConfiguration;
@@ -69,28 +66,52 @@ public class RedisTest {
         hashOperations.putAll("me", map);
         Set<Object> set = hashOperations.keys("me");
 
-//        Iterator iterator = set.iterator();
-//        while(iterator.hasNext()){
-//            Object value = hashOperations.get("me", iterator.next());
-//            System.out.println(iterator.next() + " : " + value);
-//        }
-
-        for (Object key : set) {
-            Object value = hashOperations.get("people2", key);
-            System.out.println(key+" : "+value);
+        Iterator iterator = set.iterator();
+        while (iterator.hasNext()) {
+            String key = (String) iterator.next();
+            Object value = hashOperations.get("me", key);
+            System.out.println(key + " : " + value);
         }
 
+//        for (Object key : set) {
+//            Object value = hashOperations.get("people2", key);
+//            System.out.println(key+" : "+value);
+//        }
 
     }
 
     @Test
     public void testFive() {
+        SetOperations<String, String> redisSet = redisTemplate.opsForSet();
+        String[] arr = new String[5];
+        arr[0] = "南京";
+        arr[1] = "北京";
+        arr[2] = "上海";
+        arr[3] = "北京";
+        arr[4] = "南京";
+
+        redisSet.add("city", arr);
+
+        Set<String> members = redisSet.members("city");
+        System.out.println(members);
 
     }
 
     @Test
     public void testSix() {
+        ZSetOperations<String, String > redisZSet = redisTemplate.opsForZSet();
 
+        //存值,存value的同时,还加上顺序
+        redisZSet.add("city2", "南京", 1);
+        redisZSet.add("city2", "北京", 2);
+        redisZSet.add("city2", "东京", 3);
+        redisZSet.add("city2", "西京", 4);
+        redisZSet.add("city2", "上海", 5);
+        redisZSet.add("city2", "广州", 6);
+
+        //获取范围顺序里面的值
+        Set<String> rangeByScore = redisZSet.rangeByScore("city2", 1, 10);
+        System.out.println(rangeByScore);
     }
 
 }
